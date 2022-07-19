92edd0414eaf37ceab0fd35cc9fd180e4a5251fd
pengpeng <pengpeng_on@163.com>
2022-07-19 22:23:59 +0800

    429.N叉树的层序遍

---
+++ 算法/src/main/java/com/peng/leetcode/tree/LevelOrder.java
@@ -0,0 +1,57 @@
+package com.peng.leetcode.tree;
+
+import java.util.*;
+
+/**
+ * 429-N 叉树的层序遍历
+ */
+public class LevelOrder {
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    private Queue<Node> deque = new LinkedList<>();
+    private List<List<Integer>>  result = new LinkedList<>();
+    public List<List<Integer>> levelOrder(Node root) {
+//        bfs 白能力啊
+        if(root == null){
+            return result;
+        }
+        deque.offer(root);
+        while (!deque.isEmpty()){
+            int size = deque.size();
+            ArrayList<Integer> res = new ArrayList<>();
+            while (size > 0){
+                Node poll = deque.poll();
+                if(poll == null){
+                    size--;
+                    continue;
+                }
+                res.add(poll.val);
+                for(Node node : poll.children){
+                    if (node!= null){
+                        deque.offer(node);
+                        // deque.addAll(node);
+                    }
+
+                }
+                size--;
+            }
+            result.add(res);
+
+        }
+        return result;
+    }
+}
